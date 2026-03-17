d5a2987adf16ef3247b6a1c707481a7a24aa03b1
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-17 23:18:25 +0500

    add additional manager for Post model of Blog app

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -27,6 +33,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT,
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
