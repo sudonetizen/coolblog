0a95e954e6e866bb7565add9bda95de43be01592
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-19 07:11:59 +0500

    add Slug url for Post model

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,14 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'post_list.html', {'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
 
     return render(request, 'post_detail.html', {'post': post})
