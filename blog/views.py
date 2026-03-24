8fa41d3807abec4143eb7565bf8b7ff507a933fd
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-24 19:42:28 +0500

    add post_comment view to blog app

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render, get_object_or_404
 #from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 """
 def post_list(request):
@@ -75,4 +76,15 @@ def post_share(request, post_id):
 
     return render(request, 'share.html', {'post': post, 'form': form, 'sent': sent})
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
 
+    return render(request, 'comment.html', {'post': post, 'form': form, 'comment': comment}) 
