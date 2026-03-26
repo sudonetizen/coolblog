2eff1f72585222ef2088153480b73d58f306252e
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-26 18:29:01 +0500

    add similar posts to post detail viev and template

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
+
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
@@ -49,7 +51,11 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
-    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'form': form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('same_tags', '-publish')[:4]
+
+    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
