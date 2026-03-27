ce6ecee43eaf0358c4812696b80d0d0dfec64cbb
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-27 21:13:18 +0500

    add rss feed to blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PostListView, post_list, post_detail, post_share, post_comment
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -9,6 +10,7 @@ urlpatterns = [
     #path('', PostListView.as_view() , name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
-    path('<int:post_id>/comment/', post_comment, name='post_comment')
+    path('<int:post_id>/comment/', post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
 
