835ea5a80563fa8dbd8fdb35ca31df282aa55048
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-28 03:26:10 +0500

    change db from sqlite to postgresql

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
     }
 }
 
