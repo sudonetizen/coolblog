b1fc9b66d1085026fe38c97640272f9fa357beb4
sudonetizen <sardortolibjonov1039@gmail.com>
2026-03-28 22:37:45 +0500

    add ranking and weighting to search of blog app

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -113,7 +113,16 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = (Post.published.annotate(search=SearchVector('title', 'body')).filter(search=query))
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                )
+                .filter(rank__gte=0.3)
+                .order_by('-rank')
+        )
 
     return render(request, 'search.html', {'form': form, 'query': query, 'results': results})
 
